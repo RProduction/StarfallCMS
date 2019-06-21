2a0deba695c0fc6fdd332c93eecf6ecbd61b7a86
Starfall Production <starfallproduction42@gmail.com>
2019-06-21 19:07:56 +0700

    Finish testing UserController

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -5,6 +5,9 @@ const User = use('App/Models/User');
 /** @type {import('@adonisjs/lucid/src/Database')} */
 const Database = use('Database');
 
+/** @type {import('@adonisjs/framework/src/Logger')} */
+const Logger = use('Logger');
+
 class UserController {
     
     /**
@@ -12,14 +15,22 @@ class UserController {
     * @param {import('@adonisjs/framework/src/Request')} ctx.request
     */
     async add({request}){
-        const {email, password, username, authority} = request.all();
+        const {email, password, username, authority} = request.post();
         const user = new User();
         user.username = username;
         user.email = email;
         user.password = password;
         user.authority = authority;
 
-        await user.save();
+        try{
+            Logger.info(`email: ${email}, password: ${password}, username: ${username}, authority: ${authority}`);
+            Logger.info('create new user');
+            await user.save();
+        }
+        catch(error){
+            Logger.warning('Fail to create new user');
+            Logger.warning(error);
+        }
     }
 
     /**
@@ -27,8 +38,15 @@ class UserController {
     * @param {import('@adonisjs/framework/src/Request')} ctx.request
     */
     async delete({request}){
-        const {email} = request.all();
-        await Database.table('users').where('email', email).delete();
+        const {email} = request.post();
+        try{
+            Logger.info(`delete user ${email}`);
+            await Database.table('users').where('email', email).delete();
+        }
+        catch(error){
+            Logger.warning('Fail to delete user');
+            Logger.warning(error);
+        }
     }
 
     /**
@@ -37,8 +55,16 @@ class UserController {
     * @param {import('@adonisjs/auth/src/Auth')} ctx.auth
     */
     async login({request, auth}){
-        const {email, password} = request.all();
-        await auth.attempt(email, password);
+        const {email, password} = request.post();
+
+        try{
+            Logger.info(`login email: ${email}, password: ${password}`);
+            await auth.attempt(email, password);
+        }
+        catch(error){
+            Logger.warning('Fail to login');
+            Logger.warning(error);
+        }
     }
 
     /**
@@ -46,7 +72,14 @@ class UserController {
     * @param {import('@adonisjs/auth/src/Auth')} ctx.auth
     */
     async logout({auth}){
-        await auth.logout();
+        try{
+            Logger.info(`logout`);
+            await auth.logout();
+        }
+        catch(error){
+            Logger.warning('Fail to logout');
+            Logger.warning(error);
+        }
     }
 }
 
