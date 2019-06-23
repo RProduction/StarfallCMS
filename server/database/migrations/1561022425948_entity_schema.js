a2bb739e0b229206ee533ecc5e643dbc65d4e088
Starfall Production <starfallproduction42@gmail.com>
2019-06-23 15:46:17 +0700

    Fix foreign key schema in Entity and Project

--- server/database/migrations/1561022425948_entity_schema.js
+++ server/database/migrations/1561022425948_entity_schema.js
@@ -7,8 +7,8 @@ class EntitySchema extends Schema {
   up () {
     this.create('entities', (table) => {
       table.increments()
-      table.string('entity_name', 60).notNullable().unique()
-      table.integer('project_ref').unsigned().notNullable().references('id').inTable('projects')
+      table.string('entity_name', 60).notNullable()
+      table.integer('project_id').unsigned().references('id').inTable('projects')
       table.timestamps()
     })
   }
