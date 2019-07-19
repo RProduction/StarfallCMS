e773784ad381863d3f73a1062c597af698c51406
Starfall Production <starfallproduction42@gmail.com>
2019-07-19 22:55:45 +0700

    Fully implemented EntityHook.beforeUpdate

--- server/app/Models/Hooks/EntityHook.js
+++ server/app/Models/Hooks/EntityHook.js
@@ -4,145 +4,85 @@
 /** @type {typeof import('lucid-mongo/src/LucidMongo/Model')} */
 const Document = use('App/Models/Document');
 
+/** @type {typeof import('lucid-mongo/src/LucidMongo/Database')} */
+const Database = use('Database');
+
 /** @type {import('@adonisjs/framework/src/Logger')} */
 const Logger = use('Logger');
 
+const {diff} = require('deep-diff');
+
 const EntityHook = exports = module.exports = {}
-
-// generate default query object from object of type
-function GenerateDefaultFromType(type, defaultValue){
-    Object.entries(type).forEach(([key, value])=>{
-        // check type and assign
-        if(value === 'integer'){
-            defaultValue[key] = 0;
-        }
-        else if(value === 'float'){
-            defaultValue[key] = 0;
-        }
-        else if(value === 'string'){
-            defaultValue[key] = '';
-        }
-        else if(value === 'boolean'){
-            defaultValue[key] = false;
-        }
-        else if(value.constructor === Object){
-            defaultValue[key] = {};
-
-            // traverse object
-            GenerateDefaultFromType(type[key], defaultValue[key]);
-        }
-        else if(value.constructor === Array){    
-            defaultValue[key] = [];
-        }
-    });
-}
-
-function TraverseArray(arr, defaultValue){
-    Object.entries(type).forEach(([key, value])=>{
-        // check type and assign
-        if(value === 'integer'){
-            defaultValue[key] = 0;
-        }
-        else if(value === 'float'){
-            defaultValue[key] = 0;
-        }
-        else if(value === 'string'){
-            defaultValue[key] = '';
-        }
-        else if(value === 'boolean'){
-            defaultValue[key] = false;
-        }
-        else if(value.constructor === Object){
-            defaultValue[key] = {};
-
-            // traverse object
-            GenerateDefaultFromType(type[key], defaultValue[key]);
-        }
-        else if(value.constructor == Array){    
-            defaultValue[key] = [];
-        }
-    });
-}
-
-// compare schema between old and new schema
-// return field update query
-function CompareSchema(oldSchema, newSchema, query){
-    Object.entries(newSchema).forEach(([key, value])=>{
-        let defaultObj = {};
-
-        // check if field in newSchema exist or not in old
-        // if not exist then add query set
-        // if exist then compare type 
-        if(!oldSchema[key]){
-            // check type and assign
-            if(value === 'integer'){
-                query.$set[key] = 0;
-            }
-            else if(value === 'float'){
-                query.$set[key] = 0;
-            }
-            else if(value === 'string'){
-                query.$set[key] = '';
-            }
-            else if(value === 'boolean'){
-                query.$set[key] = false;
-            }
-            else if(value.constructor === Object){
-                GenerateDefaultFromType(value, defaultObj);
-                query.$set[key] = defaultObj;
-            }
-            else if(value.constructor == Array){    
-                query.$set[key] = [];
-            }
-        }else if(value !== oldSchema[key]){
-            // check type and assign
-            if(value === 'integer'){
-                query.$set[key] = 0;
-            }
-            else if(value === 'float'){
-                query.$set[key] = 0;
-            }
-            else if(value === 'string'){
-                query.$set[key] = '';
-            }
-            else if(value === 'boolean'){
-                query.$set[key] = false;
-            }
-            else if(value.constructor === Object){
-                GenerateDefaultFromType(value, defaultObj);
-                query.$set[key] = defaultObj;
-            }
-            else if(value.constructor === Array){    
-                query.$set[key] = [];
-            }
-        }else if(value.constructor === Object){
-            // if same but object then traverse
-            CompareSchema(oldSchema[key], newSchema[key], query);
-        }else if(value.constructor === Array){
-
-        }
-    });
-
-    Object.entries(oldSchema).forEach(([key, value])=>{
-        // check if field in oldSchema exist or not in new
-        // if not exist then add query unset
-        if(!newSchema[key]){
-            query.$unset[key] = 1;
-        }
-    });
-}
 
 /** @param {Model} modelInstance*/
 // hook for update document.data key and value after schema change
 EntityHook.beforeUpdate = async (modelInstance) => {
     if(modelInstance.dirty.schema){
         let query = {$set: {}, $unset: {}};
-        CompareSchema(
+        // get difference between schema with diff library
+        const result = diff(
             modelInstance.$originalAttributes.schema, 
-            modelInstance.schema, 
-            query
+            modelInstance.schema,
+            {
+                normalize: (path, key, lhs, rhs)=>{
+                    if(lhs === 'integer'){
+                        lhs = 0;
+                    }else if(lhs === 'float'){
+                        lhs = 0;
+                    }else if(lhs === 'string'){
+                        lhs = '';
+                    }else if(lhs === 'boolean'){
+                        lhs = false;
+                    }
+
+                    if(rhs === 'integer'){
+                        rhs = 0;
+                    }else if(rhs === 'float'){
+                        rhs = 0;
+                    }else if(rhs === 'string'){
+                        rhs = '';
+                    }else if(rhs === 'boolean'){
+                        rhs = false;
+                    }
+
+                    return [lhs, rhs];
+                }
+            }
         );
-        await Document.query().update({}, query);
+
+        // and process result of diff into query
+        result.forEach(value => {
+            let key = 'data.';
+            value.path.forEach((path, index) => {
+                if(index < value.path.length-1){
+                    key = `${key}${path}.`;
+                }else{
+                    key = `${key}${path}`;
+                }
+            });
+
+            // check if change in array or not
+            if(value.kind === 'N' || value.kind === 'E'){
+                // if new or edit property in object    
+                query.$set[key] = value.rhs;
+            }else if(value.kind === 'D'){
+                // if delete property in object
+                query.$unset[key] = '';
+            }else if(value.item.kind === 'N'){
+
+            }else if(value.item.kind === 'D'){
+
+            }
+        });
+        
+        if(Object.keys(query.$set).length === 0) delete query['$set'];
+        if(Object.keys(query.$unset).length === 0) delete query['$unset'];
+        
+        const mquery = await Database.connect();
+        const affected = await mquery.collection('documents').update(
+            {entity_id: modelInstance._id}, query, {multi: true}
+        );
+        Logger.info(`modify all documents in entity ${modelInstance._id} (${affected})`);
     }
 }
 
