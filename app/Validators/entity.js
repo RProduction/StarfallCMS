a4e09bfc7544bf687efe09487124758907b83f45
Starfall Production <starfallproduction42@gmail.com>
2019-07-19 17:22:36 +0700

    Fix validator

--- app/Validators/entity.js
+++ app/Validators/entity.js
@@ -1,18 +1,21 @@
 'use strict'
 
-const {validations} = require('indicative/validator');
+/** @type {import('@adonisjs/framework/src/Logger')} */
+const Logger = use('Logger');
 
 class entity {
-  get rules () {
-    return {
-      // validation rules
-      name: [validations.required(), 
-        validations.min(1),
-        validations.max(50),
-        validations.regex(['^[a-zA-Z0-9_]+$'])
-      ]
-    }
-  }
+	get rules() {
+		return {
+			// validation rules
+			name: "required|min:1|max:50|regex:['^[a-zA-Z0-9_]+$']"
+		}
+	}
+
+	async fails(errorMessages) {
+		Logger.warning('Fail in validating entity routes');
+        Logger.warning(errorMessages);
+		return this.ctx.response.send(errorMessages);
+	}
 }
 
 module.exports = entity
