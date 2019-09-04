131ea7b0f344c2152fd782ca12f91bb8d994343a
Starfall Production <starfallproduction42@gmail.com>
2019-09-05 06:43:43 +0700

    - Change storage from using file system into using database - can support only file storage - Remove unneeded code in DocumentController.delete

--- app/Models/Hooks/ProjectHook.js
+++ app/Models/Hooks/ProjectHook.js
@@ -2,15 +2,11 @@
 /** @typedef {import('@adonisjs/lucid/src/Lucid/Model')} Model*/
 /** @typedef {typeof import('@adonisjs/lucid/src/Lucid/Model')} ModelType*/
 
-const fs = require('fs-extra');
-
 /** @type {ModelType} */
 const Entity = use('App/Models/Entity');
 
 /** @type {import('@adonisjs/framework/src/Logger')} */
 const Logger = use('Logger');
-
-const Drive = use('Drive');
 
 const Helpers = use('Helpers');
 
@@ -34,9 +30,8 @@ ProjectHook.beforeDelete = async (modelInstance) => {
         await fs.unlink(path);
     }
 
-    // delete project files folder if exist
-    if(await Drive.exists(`${modelInstance.id}`))
-        await Drive.delete(`${modelInstance.id}`);
+    // delete files in project
+    await modelInstance.files().delete();
 
     Logger.info(`delete all entities(${entities.length}) in project ${modelInstance.name}`);
 }
